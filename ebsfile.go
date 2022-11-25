09c63351e38c4ce5e17282b4deb366f5e0946b71
masahiro331 <m_fujimura@r.recruit.co.jp>
2022-11-26 03:14:39 +0900

    update(ebs): use v2 for aws-sdk-go

--- ebsfile.go
+++ ebsfile.go
@@ -7,29 +7,27 @@ import (
 	"io"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/request"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ebs"
-	"github.com/aws/aws-sdk-go/service/ebs/ebsiface"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ebs"
+	"github.com/aws/aws-sdk-go-v2/service/ebs/types"
 )
 
 type MockEBS struct {
-	ebsiface.EBSAPI
+	EBSAPI
 
 	f          *os.File
-	BlockSize  *int64
+	BlockSize  *int32
 	NextToken  *string
 	VolumeSize *int64
 }
 
-func (m MockEBS) WalkSnapshotBlocks(_ context.Context, _ *ebs.ListSnapshotBlocksInput, _ map[int64]string) (*ebs.ListSnapshotBlocksOutput, map[int64]string, error) {
-	indexLen := *m.VolumeSize / *m.BlockSize
-	var blocks []*ebs.Block
-	t := make(map[int64]string)
-	for i := int64(0); i < indexLen; i++ {
-		blocks = append(blocks, &ebs.Block{BlockIndex: aws.Int64(i)})
+func (m MockEBS) WalkSnapshotBlocks(_ context.Context, _ *ebs.ListSnapshotBlocksInput, _ map[int32]string) (*ebs.ListSnapshotBlocksOutput, map[int32]string, error) {
+	indexLen := *m.VolumeSize / int64(*m.BlockSize)
+	var blocks []types.Block
+	t := make(map[int32]string)
+	for i := int32(0); i < int32(indexLen); i++ {
+		blocks = append(blocks, types.Block{BlockIndex: aws.Int32(i)})
 		t[i] = ""
 	}
 
@@ -40,46 +38,47 @@ func (m MockEBS) WalkSnapshotBlocks(_ context.Context, _ *ebs.ListSnapshotBlocks
 	}, t, nil
 }
 
-func (m MockEBS) GetSnapshotBlockWithContext(_ aws.Context, input *ebs.GetSnapshotBlockInput, _ ...request.Option) (*ebs.GetSnapshotBlockOutput, error) {
+func (m MockEBS) GetSnapshotBlock(_ context.Context, params *ebs.GetSnapshotBlockInput, optFns ...func(*ebs.Options)) (*ebs.GetSnapshotBlockOutput, error) {
 	buf := make([]byte, *m.BlockSize)
 
-	offset := *input.BlockIndex * (*m.BlockSize)
-	n, err := m.f.ReadAt(buf, offset)
+	offset := *params.BlockIndex * (*m.BlockSize)
+	n, err := m.f.ReadAt(buf, int64(offset))
 	if err != nil {
 		return nil, err
 	}
 	return &ebs.GetSnapshotBlockOutput{
 		BlockData:  io.NopCloser(bytes.NewReader(buf)),
-		DataLength: aws.Int64(int64(n)),
+		DataLength: aws.Int32(int32(n)),
 	}, nil
 }
 
-func NewMockEBS(filePath string, blockSize, volumeSize int64) EBSAPI {
+func NewMockEBS(filePath string, blockSize int32, volumeSize int64) EBSAPI {
 	f, err := os.Open(filePath)
 	if err != nil {
 		return nil
 	}
 	return &MockEBS{
 		f:          f,
-		BlockSize:  aws.Int64(blockSize),
+		BlockSize:  aws.Int32(blockSize),
 		VolumeSize: aws.Int64(volumeSize),
 	}
 }
 
 type EBS struct {
-	ebsiface.EBSAPI
+	*ebs.Client
 }
 
 type EBSAPI interface {
-	ebsiface.EBSAPI
-	WalkSnapshotBlocks(ctx context.Context, input *ebs.ListSnapshotBlocksInput, table map[int64]string) (*ebs.ListSnapshotBlocksOutput, map[int64]string, error)
+	ListSnapshotBlocks(ctx context.Context, params *ebs.ListSnapshotBlocksInput, optFns ...func(*ebs.Options)) (*ebs.ListSnapshotBlocksOutput, error)
+	GetSnapshotBlock(ctx context.Context, params *ebs.GetSnapshotBlockInput, optFns ...func(*ebs.Options)) (*ebs.GetSnapshotBlockOutput, error)
+	WalkSnapshotBlocks(ctx context.Context, input *ebs.ListSnapshotBlocksInput, table map[int32]string) (*ebs.ListSnapshotBlocksOutput, map[int32]string, error)
 }
 
 type File struct {
 	snapshotID string
 	size       int64
-	blockSize  int64
-	blockTable map[int64]string
+	blockSize  int32
+	blockTable map[int32]string
 
 	ebsclient EBSAPI
 	cache     Cache
@@ -92,13 +91,21 @@ type Option struct {
 	AwsRegion    string
 }
 
-func New(option Option) *EBS {
-	sess := session.Must(getSession(option))
-	return &EBS{ebs.New(sess)}
+func New(option Option) (*EBS, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return nil, err
+	}
+	if option.AwsRegion != "" {
+		cfg.Region = option.AwsRegion
+	}
+	ebsClient := ebs.NewFromConfig(cfg)
+
+	return &EBS{ebsClient}, nil
 }
 
-func (e EBS) WalkSnapshotBlocks(ctx context.Context, input *ebs.ListSnapshotBlocksInput, table map[int64]string) (*ebs.ListSnapshotBlocksOutput, map[int64]string, error) {
-	output, err := e.ListSnapshotBlocksWithContext(ctx, input)
+func (e EBS) WalkSnapshotBlocks(ctx context.Context, input *ebs.ListSnapshotBlocksInput, table map[int32]string) (*ebs.ListSnapshotBlocksOutput, map[int32]string, error) {
+	output, err := e.ListSnapshotBlocks(ctx, input)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -117,7 +124,7 @@ func Open(snapID string, ctx context.Context, cache Cache, e EBSAPI) (*io.Sectio
 	input := &ebs.ListSnapshotBlocksInput{
 		SnapshotId: &snapID,
 	}
-	output, table, err := e.WalkSnapshotBlocks(ctx, input, make(map[int64]string))
+	output, table, err := e.WalkSnapshotBlocks(ctx, input, make(map[int32]string))
 	if err != nil {
 		return nil, err
 	}
@@ -141,7 +148,7 @@ func (f *File) ReadAt(p []byte, off int64) (n int, err error) {
 	index, dataOffset := f.translateOffset(off)
 	token, ok := f.blockTable[index]
 	if !ok {
-		if f.blockSize-dataOffset < int64(len(p)) {
+		if f.blockSize-dataOffset < int32(len(p)) {
 			return int(f.blockSize - dataOffset), nil
 		} else {
 			return len(p), nil
@@ -163,10 +170,10 @@ func (f *File) ReadAt(p []byte, off int64) (n int, err error) {
 		return 0, fmt.Errorf("invalid block size: len(buf): %d != f.blockSize: %d", len(buf), f.blockSize)
 	}
 
-	if f.blockSize-dataOffset < int64(len(p)) {
+	if f.blockSize-dataOffset < int32(len(p)) {
 		return copy(p, buf[dataOffset:]), nil
 	} else {
-		return copy(p, buf[dataOffset:dataOffset+int64(len(p))]), nil
+		return copy(p, buf[dataOffset:dataOffset+int32(len(p))]), nil
 	}
 }
 
@@ -174,20 +181,20 @@ func (f *File) Size() int64 {
 	return f.size
 }
 
-func (f *File) translateOffset(offset int64) (int64, int64) {
-	index := offset / f.blockSize
-	dataOffset := offset % f.blockSize
+func (f *File) translateOffset(offset int64) (int32, int32) {
+	index := offset / int64(f.blockSize)
+	dataOffset := offset % int64(f.blockSize)
 
-	return index, dataOffset
+	return int32(index), int32(dataOffset)
 }
 
-func (f *File) read(index int64, token string) ([]byte, error) {
+func (f *File) read(index int32, token string) ([]byte, error) {
 	input := &ebs.GetSnapshotBlockInput{
 		SnapshotId: &f.snapshotID,
 		BlockIndex: &index,
 		BlockToken: &token,
 	}
-	o, err := f.ebsclient.GetSnapshotBlockWithContext(f.ctx, input)
+	o, err := f.ebsclient.GetSnapshotBlock(f.ctx, input)
 	if err != nil {
 		return nil, err
 	}
@@ -198,26 +205,4 @@ func (f *File) read(index int64, token string) ([]byte, error) {
 	defer o.BlockData.Close()
 
 	return buf, nil
-}
-
-func getSession(option Option) (*session.Session, error) {
-	// create custom credential information if option is valid
-	if option.AwsSecretKey != "" && option.AwsAccessKey != "" && option.AwsRegion != "" {
-		return session.NewSessionWithOptions(
-			session.Options{
-				Config: aws.Config{
-					Region: aws.String(option.AwsRegion),
-					Credentials: credentials.NewStaticCredentialsFromCreds(
-						credentials.Value{
-							AccessKeyID:     option.AwsAccessKey,
-							SecretAccessKey: option.AwsSecretKey,
-						},
-					),
-				},
-			})
-	}
-	// use shared configuration normally
-	return session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	})
 }
